b3094d66fc7e1b398c9735e70df76893dc5997a7
ylh96 <ylh96@10.0.0.243>
2016-10-27 23:24:37 -0700

    hi

--- src/CSE461P2.java
+++ src/CSE461P2.java
@@ -18,7 +18,7 @@ import java.util.Random;
  */
 
 /**
- * @author ylh96
+ * @author Yilun Hua (1428927) Shen Wang (1571169)
  *
  */
 public class CSE461P2 {
@@ -35,408 +35,69 @@ public class CSE461P2 {
 			return;
 		}
 		int port_num = Integer.parseInt(args[0]);
-		DatagramSocket serverSocket = new DatagramSocket(port_num); 
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(port_num);
+		} catch (IOException e) {
+			System.out.println("Could not listen on port " + port_num);
+			System.exit(-1);
+		}
 		while(true){
-			byte[] receiveData = new byte[24];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			serverSocket.receive(receivePacket);
+			Socket client = null;
+			try {
+				client = serverSocket.accept();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			new Thread(
-					new Client_handler(
-							receivePacket, serverSocket)
+					new Client_handler(serverSocket, client)
 						).start(); 
 		  	}
 	}
 	
 	static class Client_handler implements Runnable {
-
-		public DatagramPacket receivePacket;
-		public DatagramSocket serverSocket;
-		public int student_id = 0;
-		public int[] secrets = new int[4];
 		
-		public Client_handler(DatagramPacket receivePacket, DatagramSocket serverSocket) {
-			this.receivePacket = receivePacket;
-			this.serverSocket = serverSocket;
+		public ServerSocket serverSocket;
+		public Socket socket;
+		
+		public Client_handler(ServerSocket serverSocket, Socket socket) {
+			serverSocket = serverSocket;
+			socket = socket;
 		}
 
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
 			//generate_secret();
-			int[] data = null;
-			try {
-				data = stageA();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			int next_port = 0;
-			try {
-				next_port = stageB(data);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			ServerSocket new_server = null;
-			try {
-				new_server = new ServerSocket(next_port);
-			} catch (IOException e) {
-				System.out.println("Could not listen on port " + next_port);
-				System.exit(-1);
-			}
-			Socket client = null;
-			try {
-				client = new_server.accept();
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			ByteBuffer data_from_c = null;
-			try {
-				data_from_c = stageC(client);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			try {
-				stageD(data_from_c, client);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			
 		}
 
-		public void stageD(ByteBuffer from_stage_c, Socket clientSocket) throws IOException {
-			int num2 = from_stage_c.getInt(0);
-			int len2 = from_stage_c.getInt(4);
-			byte c = from_stage_c.get(8);
-			System.out.println("num of packets: " + num2);
-			System.out.println("length d: " + len2);
-			System.out.println("char origin: " + c);
-			// get input from client
+		// get request from the client
+		public void client_to_proxy() throws IOException {
 			
-			System.out.println("apple" + clientSocket.isClosed());
-			InputStream in;
-			DataInputStream dis = null;
-			int counter = 0;
-			while (counter != num2) {
-				/*
-				InputStream in;
-				DataInputStream dis = null;
-				*/
-				try {
-					in = clientSocket.getInputStream();
-					dis = new DataInputStream(in);
-					System.out.println("first try");
-				} catch (IOException e) {
-					System.out.println("in or out failed");
-					System.exit(-1);
-				}
-				System.out.println("enter while loop");
-				byte[] data = new byte[12 + len2 + padding_bytes(len2)];
-				dis.read(data);
-				ByteBuffer in_data = ByteBuffer.wrap(data);
-				System.out.println("stage d len: " + in_data.getInt(0));
-				System.out.println("stage d psecret: " + in_data.getInt(4));
-				System.out.println("stage d step num: " + in_data.getShort(8));
-				System.out.println("stage d sid: " + in_data.getShort(10));
-				//System.out.println("stage d tcp_port a to b: " + port_num);
-				
-				// verify whether the secret is 0
-				if (!verify_header(secrets[2], in_data)) {
-					System.out.println(counter);
-					System.out.println("header format problem");
-					System.exit(-1);
-				}
-				System.out.println("actual c: " + c);
-				for (int i = 0; i < len2; i++) {
-					byte temp = in_data.get(i + 12);
-					
-					System.out.println("char get: " + temp);
-					if (temp != c) {
-						System.out.println(i);
-						System.out.println("wrong message");
-						System.exit(-1);
-					}
-				}
-				counter++;
-			}
-			// send data to client
-			OutputStream out;
-			DataOutputStream dos = null;
-			try {
-				out = clientSocket.getOutputStream();
-				dos = new DataOutputStream(out);
-			} catch (IOException e) {
-				System.out.println("Read failed");
-				System.exit(-1);
-			}
-			ByteBuffer sendData = ByteBuffer.allocate(16);
-			generate_header(secrets[2], 4, sendData);
-			sendData.putInt(12, secrets[3]);
-			dos.write(sendData.array(), 0, 16);
 		}
-
-		public ByteBuffer stageC(Socket clientSocket) throws IOException {
-			// send data to client
-			OutputStream out;
-			DataOutputStream dos = null;
-			try {
-				out = clientSocket.getOutputStream();
-				dos = new DataOutputStream(out);
-			} catch (IOException e) {
-				System.out.println("Read failed");
-				System.exit(-1);
-			}
-
-			int actual_payload = 13;
-			int padding_byte = padding_bytes(13);
-			ByteBuffer sendData = ByteBuffer.allocate(12 + actual_payload + padding_byte);
-			generate_header(secrets[1], actual_payload, sendData);
-			Random rand = new Random();
-			int num2 = rand.nextInt(99) + 1;
-			int len2 = rand.nextInt(499) + 1;
-			// create a random c
-			byte[] c = new byte[1];
-			rand.nextBytes(c);
-			sendData.putInt(12, num2).putInt(16, len2).putInt(20, secrets[2]).put(24, c[0]);
-			// stuffing with padding
-			for (int i = 0; i < padding_byte; i++) {
-				byte temp = 0;
-				sendData.put(i + 25, temp);
-			}
-			byte[] send = sendData.array();
-			dos.write(send, 0, 12 + actual_payload + padding_byte);
-			ByteBuffer from_stage_c = ByteBuffer.allocate(9);
-			from_stage_c.putInt(num2).putInt(len2).put(c[0]);
-			System.out.println("char in stage c: " + c[0]);
-			return from_stage_c;
+		
+		// get client's request to the server
+		public void proxy_to_server() throws IOException {
+			
 		}
-
-		@SuppressWarnings("resource")
-		public int stageB(int[] from_stage_a) throws Exception {
-			int send_num = from_stage_a[0];
-			int len = from_stage_a[1];
-			int port_num = from_stage_a[2];
-			DatagramSocket clientSocket = new DatagramSocket(port_num);
-			int counter = 0;
-			int in_data_size = 12 + len + 4 + padding_bytes(len + 4);
-			InetAddress IPAddress = null;
-			int port = 0;
-			while (counter != send_num) {
-				DatagramPacket receivePacket;
-				try{ 
-					System.out.println("entering try catch");
-					// set retransmit interval
-					clientSocket.setSoTimeout(500);
-					System.out.println("sent");
-					byte[] receiveData = new byte[in_data_size];
-					receivePacket = new DatagramPacket(receiveData, receiveData.length);
-					System.out.println("socket connect status: " + clientSocket.isConnected());
-					System.out.println("start receive");
-					clientSocket.receive(receivePacket);
-					IPAddress = receivePacket.getAddress();
-					port = receivePacket.getPort();
-					System.out.println("receive end");
-				} catch (SocketTimeoutException e) {
-					System.out.println("continue");
-					continue;
-				}
-				byte[] receive_ack = receivePacket.getData();
-				ByteBuffer ack_pack = ByteBuffer.wrap(receive_ack);
-				if (ack_pack.getInt(0) - 4 != len || !verify_header(secrets[0], ack_pack)) {
-					System.out.println("header format problem");
-					System.exit(-1);
-				}
-				
-				System.out.println("ack receive header len: " + ack_pack.getInt(0));
-				System.out.println("ack receive header psecret b: " + ack_pack.getInt(4));
-				System.out.println("ack receive header step num b: " + ack_pack.getShort(8));
-				System.out.println("ack receive header sid b: " + ack_pack.getShort(10));
-				
-				int packet_id = ack_pack.getInt(12);
-				System.out.println("check pack_id: " + packet_id);
-				for (int i = 0; i < len; i++) {
-					byte temp = ack_pack.get(i + 16);
-					if (temp != (byte) 0) {
-						System.out.println("payload content is not correct!");
-						continue;
-					}
-				}
-				
-				// send data
-				ByteBuffer sendData = ByteBuffer.allocate(16);
-				generate_header(secrets[0], 4, sendData);
-				
-				
-				// prepare packet
-				System.out.println("len a to b: " + len);
-				System.out.println("len b: " + sendData.getInt(0));
-				System.out.println("psecret b: " + sendData.getInt(4));
-				System.out.println("step num b: " + sendData.getShort(8));
-				System.out.println("sid b: " + sendData.getShort(10));
-				sendData.putInt(12, packet_id);
-				System.out.println("packid b: " + sendData.getInt(12));
-				byte[] send = sendData.array();
-				DatagramPacket sendPacket_b = new DatagramPacket(send, send.length, IPAddress, port);
-				clientSocket.send(sendPacket_b);
-				counter++;
-			}
-			ByteBuffer sendData = ByteBuffer.allocate(20);
-			generate_header(secrets[0], 8, sendData);
-			// ByteBuffer content = ByteBuffer.allocate(8);
-			Random rand = new Random();
-			int tcp_port = rand.nextInt(49000) + 1024;
-			while (port_num - 54321 == 0) {
-				tcp_port = rand.nextInt(49000) + 1024;
-			}
-			System.out.println(secrets[1]);
-			sendData.putInt(12, tcp_port).putInt(16, secrets[1]);
+		
+		// get data from the server and send that to client
+		public void proxy_to_client() throws IOException {
 			
-			System.out.println("len: " + sendData.getInt(0));
-			System.out.println("psecret: " + sendData.getInt(4));
-			System.out.println("step num: " + sendData.getShort(8));
-			System.out.println("sid: " + sendData.getShort(10));
-			System.out.println("tcp_port a to b: " + port_num);
-			
-			System.out.println("tcp port: " + sendData.getInt(12));
-			System.out.println("secretB: " + sendData.getInt(16));
-			byte[] send = sendData.array();
-			DatagramPacket sendPacket = new DatagramPacket(send, send.length, IPAddress, port);
-			clientSocket.send(sendPacket);
-			return tcp_port;
-
 		}
-
-		public int[] stageA() throws Exception {
-			// get input from client
-			// byte[] sendData = new byte[28];
-			ByteBuffer sendData = ByteBuffer.allocate(28);
-			InetAddress IPAddress = receivePacket.getAddress();
-			int port = receivePacket.getPort();
-			// extract data from server packet
-			byte[] fromClient = receivePacket.getData();
-			ByteBuffer bf = ByteBuffer.wrap(fromClient);
-			
-			//student_id = 927;
-			student_id = bf.getShort(10);
-			generate_secret();
-			// verify whether the secret is 0
-			if (!verify_header(0, bf)) {
-				System.out.println("header format problem");
-				System.exit(-1);
-			}
-			String secret_phase = "hello world\0";
-			int phase_byte_length = secret_phase.getBytes().length;
-			assert (phase_byte_length == 12);
-			String incoming_phase = "";
-			for (int i = 0; i < phase_byte_length; i++) {
-				byte c = 0;
-				try {
-					c = bf.get(i + 12);
-				} catch (ArrayIndexOutOfBoundsException e) {
-					System.out.println("wrong message");
-					System.exit(-1);
-				}
-				incoming_phase += (char) c;
-			}
-			if (!incoming_phase.equals(secret_phase)) {
-				System.out.println("wrong message");
-				System.exit(-1);
-			}
-
-			// now send packet back to client
-
-			generate_header(0, 16, sendData);
-			System.out.println("duck generate header");
-			System.out.println(sendData.getShort(8));
-			System.out.println(sendData.getShort(10));
-			
-			Random rand = new Random();
-			int port_num = rand.nextInt(49000) + 1024;
-			System.out.println("port num: " + port_num);
-			while (port_num - 54321 == 0) {
-				port_num = rand.nextInt(49000) + 1024;
-			}
-			int num_send = rand.nextInt(99) + 1;
-			System.out.println("num send: " + num_send);
-			int len = rand.nextInt(499) + 1;
-			System.out.println("len: " + len);
-			System.out.println("appleAA: " + secrets[0]);
-			sendData.putInt(num_send).putInt(len).putInt(port_num).putInt(secrets[0]);
-			
-			System.out.println("len A: " + sendData.getInt(0));
-			System.out.println("psecret A: " + sendData.getInt(4));
-			System.out.println("step num A: " + sendData.getShort(8));
-			System.out.println("sid A: " + sendData.getShort(10));
-			
-			System.out.println("NUM A: " + sendData.getInt(12));
-			System.out.println("LEN A: " + sendData.getInt(16));
-			System.out.println("UDP PORT A: " + sendData.getInt(20));
-			System.out.println("SECRET A: " + sendData.getInt(24));
-            byte[] send = sendData.array();
-			DatagramPacket sendPacket = new DatagramPacket(send, send.length, IPAddress, port);
-			serverSocket.send(sendPacket);
-			
-			/*System.out.println("len A: " + bfA.getInt(0));
-			System.out.println("psecret A: " + bfA.getInt(4));
-			System.out.println("step num A: " + bfA.getInt(8));
-			System.out.println("sid A: " + bfA.getInt(10));
-			
-			System.out.println("NUM A: " + bfA.getInt(12));
-			System.out.println("LEN A: " + bf.getInt(16));
-			System.out.println("UDP PORT A: " + bf.getInt(20));
-			System.out.println("SECRET A: " + bf.getInt(24));*/
-			int[] from_stage_a = {num_send, len, port_num };
-			return from_stage_a;
-         }
-
-		// we only need to verify psecret, step num, and student ID last 3
-		// digits
-		public boolean verify_header(int psecret, ByteBuffer head_buf) {
-			int psecret_get = head_buf.getInt(4);
-			System.out.println("secret: " + psecret_get);
-			int step_num_get = head_buf.getShort(8);
-			System.out.println("step_num: " + step_num_get);
-			int student_id_get = head_buf.getShort(10);
-			System.out.println("student_id: " + student_id_get);
-			System.out.println("enter verify header");
-			if (psecret != psecret_get) {
-				System.out.println("psecret wrong");
-				return false;
-			}
-			if (step_num_get != (short) 1) {
-				System.out.println("step num wrong");
-				return false;
-			}
-			if (student_id_get != (short) student_id) {
-				System.out.println("sid wrong");
-				return false;
-			}
+		
+		// verify header
+		public boolean verify_header(ByteBuffer head_buf) {
 			return true;
 		}
 
-		public void generate_header(int secret, int content_len, ByteBuffer header) {
-			//ByteBuffer header = ByteBuffer.allocate(12);
-			short step_num = 2;
-			//short student = 927;
-			header.putInt(content_len).putInt(secret).putShort(step_num).putShort((short)student_id);
-			System.out.println("header step header: " + header.getInt(0));
-			System.out.println("header step header: " + header.getInt(4));
-			System.out.println("duck");
-			System.out.println(header.getShort(8));
-			System.out.println(header.getShort(10));
-			//return header.array();
+		// generate http header
+		public void generate_header() {
 		}
-
-		public void generate_secret() {
-			Random rand = new Random();
-			secrets[0] = rand.nextInt(student_id);
-			secrets[1] = rand.nextInt(student_id);
-			secrets[2] = rand.nextInt(student_id);
-			secrets[3] = rand.nextInt(student_id);
-			/*secrets[0] = 11;
-			secrets[1] = 12;
-			secrets[2] = 13;
-			secrets[3] = 14;*/
-		}
-
+		
+		// may need to pad
 		public int padding_bytes(int length) {
 			if (length % 4 == 0) {
 				return 0;
