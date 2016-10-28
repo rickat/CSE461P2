7aee99ac94a6139fd1d161aefce9302bd93be72a
ylh96 <ylh96@10.0.0.243>
2016-10-28 00:54:04 -0700

    a

--- src/CSE461P2.java
+++ src/CSE461P2.java
@@ -51,18 +51,17 @@ public class CSE461P2 {
 				e1.printStackTrace();
 			}
 			new Thread(
-					new Client_handler(serverSocket, client)
+					new Client_handler(client)
 						).start(); 
 		  	}
 	}
 	
 	static class Client_handler implements Runnable {
 		
-		public ServerSocket serverSocket;
+		// public ServerSocket serverSocket;
 		public Socket clientSocket;
 		
-		public Client_handler(ServerSocket serverSocket, Socket socket) {
-			serverSocket = serverSocket;
+		public Client_handler(Socket socket) {
 			clientSocket = socket;
 		}
 
@@ -70,7 +69,13 @@ public class CSE461P2 {
 		public void run() {
 			// TODO Auto-generated method stub
 			//generate_secret();
-			
+			StringBuilder sb;
+			try {
+				sb = client_to_proxy();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
 		// get request from the client
@@ -101,9 +106,30 @@ public class CSE461P2 {
 			return clientData;
 		}
 		
-		// get client's request to the server
-		public void proxy_to_server() throws IOException {
+		// get client's request to the server and receive the server's respond
+		public StringBuilder proxy_to_server(StringBuilder sb) throws IOException {
 			
+			
+			int read = -1;
+			byte[] buffer = new byte[5*1024]; // a read buffer of 5KiB
+			byte[] readData;
+			StringBuilder serverData = new StringBuilder();
+			String readDataText;
+			boolean first = true;
+			while ((read = clientSocket.getInputStream().read(buffer)) > -1) {
+			    readData = new byte[read];
+			    System.arraycopy(buffer, 0, readData, 0, read);
+			    readDataText = new String(readData,"UTF-8"); // assumption that client sends data UTF-8 encoded
+			    // System.out.println("message part recieved:" + redDataText);
+			    if (first) {
+			    	int idx = readDataText.indexOf("\r\n\r\n");
+			    	// change 1.1 to 1.0
+			    	readDataText = readDataText.substring(0, idx - 1) + "0" + readDataText.substring(idx);
+			    	first = false;
+			    }
+			    serverData.append(readDataText);
+			}
+			return serverData;
 		}
 		
 		// get data from the server and send that to client
